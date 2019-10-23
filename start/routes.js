f9936ca01bc6556bf5a355fbd2fe0beba3a14d50
Tiago <tiagolc@live.com>
2019-10-23 01:41:05 -0300

    Fist commit

---
+++ start/routes.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const Route = use('Route')
+
+Route.post('users', 'UserController.store')
+Route.post('sessions', 'SessionController.store')
+
+Route.post('passwords', 'ForgotPasswordController.store')
+Route.put('passwords', 'ForgotPasswordController.update')
+
+Route.get('/files/:id', 'FileController.show')
+Route.post('/files', 'FileController.store')
