7c88ddb9a287c0d0f6fefdb6562460c2cb9a7fc6
Tiago <tiagolc@live.com>
2019-10-24 22:17:32 -0300

    RestApi Just finished

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,12 +1,21 @@
 'use strict'
 
+const Database = use('Database')
 const User = use('App/Models/User')
 
 class UserController {
   async store ({ request }) {
     const data = request.only(['username', 'email', 'password'])
 
-    const user = await User.create(data)
+    const addresses = request.input('addresses')
+
+    const trx = await Database.beginTransaction()
+
+    const user = await User.create(data, trx)
+
+    await user.addresses().createMany(addresses, trx)
+
+    await trx.commit()
 
     return user
   }
